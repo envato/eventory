84803e78d8a4e48aff1af62442e3eb190ad261b2
Steve Hodgkiss <steve@hodgkiss.me>
2018-02-12 21:20:31 +1100

    Check in recorded_event helper

--- spec/support/events.rb
+++ spec/support/events.rb
@@ -10,3 +10,27 @@ end
 class ItemStarred < PostgresEventStore::Event
   attribute :item_id
 end
+
+module EventHelpers
+  def recorded_event(id: SecureRandom.uuid,
+                     stream_id: SecureRandom.uuid,
+                     stream_version: 1,
+                     type: 'test',
+                     data: {},
+                     recorded_at: Time.now.utc)
+    @number ||= 0
+    PostgresEventStore::RecordedEvent.new(
+      number: @number += 1,
+      id: id,
+      stream_id: stream_id,
+      stream_version: stream_version,
+      type: type,
+      data: data,
+      recorded_at: recorded_at
+    )
+  end
+end
+
+RSpec.configure do |config|
+  config.include(EventHelpers)
+end
