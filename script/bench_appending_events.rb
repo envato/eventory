3c9073206df9056ad5aedc9b07ea30d35378d36e
Steve Hodgkiss <steve@hodgkiss.me>
2018-03-03 18:12:04 +1100

    Rename to bench_appending_events.rb

--- script/bench_appending_events.rb
+++ script/bench_appending_events.rb
@@ -10,27 +10,34 @@ db.extension(:pg_array)
 db.extension(:pg_json)
 db.logger = Logger.new(STDOUT) if ENV['LOG']
 
+puts "Resetting data"
 db.run 'truncate table events'
 db.run 'update event_counter set number = 0'
+puts "Vacuming tables"
+db.run 'VACUUM FULL event_counter'
+db.run 'VACUUM FULL events'
 
 event_store = Eventory::EventStore.new(database: db)
 
-CONCURRENCY = 5
-EVENTS = 10_000
+CONCURRENCY = 10
+TOTAL_EVENTS = 50_000
+EVENTS = TOTAL_EVENTS / CONCURRENCY
 
-stream_ids = 10.times.map { SecureRandom.uuid }
+stream_ids = 200.times.map { SecureRandom.uuid }
 
 db.disconnect
 
+print "Forking #{CONCURRENCY} processes... "
 start = Time.now
 
 CONCURRENCY.times do
   fork do
     EVENTS.times do
-      event_store.save(stream_ids.sample, Eventory::EventData.new(type: 'test', data: { 'a' => '1' }))
+      event_store.append(stream_ids.sample, Eventory::EventData.new(type: 'test', data: { 'a' => '1' }))
     end
   end
 end
+puts "Done"
 Process.waitall
 end_time = Time.now
 time_taken = end_time - start
